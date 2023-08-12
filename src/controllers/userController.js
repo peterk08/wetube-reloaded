eac7f7e2b244f0ae395a49b8535b5dc22da5edf7
peterkkhan <juna08@naver.com>
2023-08-12 15:40:00 +0900

    Initial commit

---
+++ src/controllers/userController.js
@@ -0,0 +1,50 @@
+import User from "../models/User";
+
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join"});
+
+export const postJoin = async (req, res) => {
+    const {name, username, email, password, password2, location} = req.body;
+    const pageTitle = "Join";
+    if(password !== password2){
+        return res.status(400).render("join", {
+            pageTitle,
+            errorMessage: "Password confirmation does not match.",
+        });
+    };
+    const exists = await User.exists({ $or: [{username}, {email}] });
+    if(exists) {
+        return res.status(400).render("join", {
+            pageTitle,
+            errorMessage: "This username/email is already taken.",
+        });
+    };
+    
+    try {
+        await User.create({
+            name, username, email, password, location,
+        });
+        return res.redirect("/login");
+    } catch(error) {
+        return res.status(400).render("join", {
+            pageTitle: "Upload Video",
+            errorMessage: error._message,
+        });
+    };
+};
+export const getLogin = (req, res) => res.render("login", { pageTitle: "Login"});
+
+export const postLogin = async (req, res) => {
+    const {username, password} = req.body;
+    const exists = await User.exists({username});
+    if (!exists) {
+        return res.status(400).render("login", {pageTitle: "Login", errorMessage:"An account username does not exists.",});
+    }
+    res.end();
+}
+
+export const edit = (req, res) => res.send("edit user");
+export const remove = (req, res) => res.send("remove user");
+export const logout = (req, res) => res.send("logout");
+export const see = (req, res) => res.send("see user");
+
---
+++ src/server.js
@@ -0,0 +1,28 @@
+import "./db";
+import Video from "./models/Video";
+import express from "express";
+import morgan from"morgan";
+import rootRouter from "./routers/rootRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
+
+const PORT = 4000;
+
+const app = express();
+const logger = morgan("dev");
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use(express.urlencoded({extended: true}));
+app.use("/", rootRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
+/*const handleListening = () => 
+    console.log(`✅Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);*/
+
+export default app;
