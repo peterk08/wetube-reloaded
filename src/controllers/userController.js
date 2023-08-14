041d2402cd5095740b73388cb27fc1f45f6d2502
peterkhan <juna08@naver.com>
2023-08-14 10:05:11 +0900

    7.11Recap
    nomad corder

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -47,6 +47,8 @@ export const postLogin = async (req, res) => {
         return res.status(400).render("login", {pageTitle, errorMessage:"Wrong password",});
     }
     console.log("LOG USER IN! COMING SOON!");
+    req.session.loggedIn = true;
+    req.session.user = user;
     return res.redirect("/");
 }
 
