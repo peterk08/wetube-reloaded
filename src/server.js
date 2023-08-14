20c0e9b23771a2eed88331732c1b32cae8eb0fc8
peterkhan <juna08@naver.com>
2023-08-14 11:50:26 +0900

    7.15
    env setting

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-import "./db";
 import express from "express";
+//import "./db";
 import morgan from"morgan";
 import session from "express-session";
 import MongoStore from "connect-mongo";
@@ -21,10 +21,10 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(
   session ({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
-    store: MongoStore.create({mongoUrl: "mongodb://127.0.0.1:27017/wetube"}),
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({mongoUrl:process.env.DB_URL}),
    })
 );
 
