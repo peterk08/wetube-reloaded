72be0a3072ee26d0ed6a42e19906e51f09a3a2fe
peterkhan <juna08@naver.com>
2023-08-20 00:14:37 +0900

    8.9 Video Uploads
    Video

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,21 +2,21 @@ import express from "express";
 import {getEdit, postEdit, logout, see, startGithubLogin, finishGithubLogin, getChangePassword, postChangePassword} from "../controllers/userController";
 import { get } from "mongoose";
 import {
+    avatarUpload,
     protectorMiddleware,
     publicOnlyMiddleware,
-    uploadFiles,
   } from "../middlewares";
 
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
-userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(uploadFiles.single("avatar"), postEdit);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(avatarUpload.single("avatar"), postEdit);
 userRouter
   .route("/edit")
   .all(protectorMiddleware)
   .get(getEdit)
-  .post(uploadFiles.single("avatar"), postEdit);
+  .post(avatarUpload.single("avatar"), postEdit);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(":id", see)
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {watch, getEdit, postEdit, getUpload, postUpload, deleteVideo,} from "../controllers/videoController";
-import { protectorMiddleware,  } from "../middlewares";
+import { protectorMiddleware, videoUpload,  } from "../middlewares";
 
 
 
@@ -9,7 +9,7 @@ const videoRouter = express.Router();
 videoRouter.route("/:id([0-9a-f]{24})").get(watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
 videoRouter.route("/:id([0-9a-f]{24})/delete").all(protectorMiddleware).get(deleteVideo)
-videoRouter.route("/upload").all(protectorMiddleware).get(getUpload).post(postUpload);
+videoRouter.route("/upload").all(protectorMiddleware).get(getUpload).post(videoUpload.single("video"), postUpload);
 
 
 
