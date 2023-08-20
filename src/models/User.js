dde98c974ab1fd2fcaf7cb9cd2a59d5f9e82754a
peterkhan <juna08@naver.com>
2023-08-20 22:48:39 +0900

    User Profile Done
    No error
    one bug: no upload to location

--- src/models/User.js
+++ src/models/User.js
@@ -9,13 +9,14 @@ const userSchema = new mongoose.Schema({
     password: { type: String,},
     name: {type: String, required: true},
     loacation: String,
+    videos: [{type:mongoose.Schema.Types.ObjectId, ref:"Video"},]
 });
 
 userSchema.pre('save', async function(){
-    console.log("Users password:", this.password);
+    if (this.isModified("password")){
     this.password = await bcrypt.hash(this.password, 5);
-    console.log("Hashed password:", this.password);
-})
+    }
+});
 
 const User = mongoose.model("User", userSchema);
 export default User;
