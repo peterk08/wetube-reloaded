2f428a9aa73c8cd77712f8fecc60f44cd6efdb55
peterkhan <juna08@naver.com>
2023-08-21 22:10:15 +0900

    9.7 webpack done
    no error
    done!

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 const path = require("path");
+
+
 
 module.exports = {
   entry: "./src/client/js/main.js",
   mode: "development",
+  watch: true,
+  plugins: [new MiniCssExtractPlugin({
+    filename: "css/styles.css",
+  })],
   output: {
-    filename: "main.js",
-    path: "./assets/js",
-    path: path.resolve(__dirname, "assets", "js"),
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
+    clean: true,
   },
   module: {
     rules: [
@@ -19,6 +27,10 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      }
     ],
   },
 };
