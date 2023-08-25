996b1582c89938b6bfdf7b9f0aa95711814dc96b
peterkhan <juna08@naver.com>
2023-08-26 00:24:32 +0900

    11.2 done
    lego

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,23 +2,30 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
-const handlePlay = (e) => {
-    // if the video is playing, pause it
-    if(video.pause) {
-        playBtn.innerText = "Pause";
+const handlePlayClick = (e) => {
+    if(video.paused) {
         video.play();
     } else {
-       
         video.pause();
     }
+    playBtn.innerText = video.paused ? "Play" : "Pause";
 }
 
 const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
 
-const handleMute = (e) => {}
+const handleMute = (e) => {
+    if (video.muted) {
+        video.muted = false;
+    } else {
+        video.muted = true;
+    }
+    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+    volumeRange.value = video.muted ? 0 : 0.5;
+};
 
-playBtn.addEventListener("click", handlePlay);
+
+playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
