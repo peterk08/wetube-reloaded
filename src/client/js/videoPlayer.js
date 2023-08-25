ac04a74a9d2ea34778752eed226ef2440863e8fa
peterkhan <juna08@naver.com>
2023-08-25 23:58:10 +0900

    11.1 almost done
    almost

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,24 @@
-console.log("Video Player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlay = (e) => {
+    // if the video is playing, pause it
+    if(video.pause) {
+        playBtn.innerText = "Pause";
+        video.play();
+    } else {
+       
+        video.pause();
+    }
+}
+
+const handlePause = () => (playBtn.innerText = "Play");
+
+const handleMute = (e) => {}
+
+playBtn.addEventListener("click", handlePlay);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
