4b0d98f4f7c998a3aa2199dcf208cc73f7ae51d7
peterkhan <juna08@naver.com>
2023-08-27 21:54:54 +0900

    11.5 Time Formatting
    format

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,9 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 
 let volumeValue = 0.5;
@@ -40,7 +41,19 @@ const handleVolumeChange = (event) => {
     video.volume = value;
 };
 
+const formatTime = (seconds) => new Date(seconds *1000).toISOString().substring(11, 19);
+
+const handleLoadedMetadata = () => {
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+}
+
+const handleTimeUpdate = () => {
+    currentTime.innerText = formatTime(Math.floor(video.currentTime));
+    console.log(video.currentTime);
+}
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange)
+video.addEventListener("loadedmetadata",handleLoadedMetadata )
+video.addEventListener("timeupdate",handleTimeUpdate )
