07faf07fcf6c23b8408d7c7b71730fffd84d9b60
peterkhan <juna08@naver.com>
2023-08-29 22:06:50 +0900

    12.0 register View Controller
    done

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import apiRouter from './routers/apiRouter';
 import { localsMiddleware } from "./middlewares";
 
 
@@ -42,6 +43,7 @@ app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
+app.use("/api", apiRouter);
 
 /*const handleListening = () => 
     console.log(`✅Server listening on port http://localhost:${PORT}`);
