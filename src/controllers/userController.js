69281be3f939264f11c843e978705ecfe7070541
peterkhan <juna08@naver.com>
2023-09-05 22:31:50 +0900

    15.1 Sending Message
    done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -165,6 +165,7 @@ export const postEdit = async (req, res) => {
 
 export const getChangePassword = (req, res) =>{
   if (req.session.user.socialOnly === true) {
+    req.flash("error", "Can't change password.");
     return res.redirect("/");
   }
   return res.render("users/change-password", {pageTitle:"Change Password"})
@@ -188,6 +189,7 @@ export const postChangePassword = async (req, res) =>{
   }
   user.password = newPassword;
   await user.save();
+  req.flash("info", "Password updated");
   return res.redirect("/users/logout");
 }
 
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 //import "./db";
 import morgan from"morgan";
 import session from "express-session";
+import flash from "express-flash";
 import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
@@ -21,6 +22,7 @@ app.use((req, res, next) => {
     res.header("Cross-Origin-Opener-Policy", "same-origin");
     next();
   });
+
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
@@ -43,7 +45,7 @@ app.use(
         next();
     });
 });*/
-
+app.use(flash());
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"));
