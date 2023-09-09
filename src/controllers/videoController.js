7f776662589ab2aedb74a54683b5cf84accfe030
peterkhan <juna08@naver.com>
2023-09-09 09:37:58 +0900

    16.9 done
    challenge subject is delete comment seciotn.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -9,6 +9,7 @@ export const home = async(req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id).populate("owner").populate("comments");
+  console.log(video);
   if(!video){
     return res.render("404", {pageTitle:"Video not found."});
   }
@@ -131,5 +132,5 @@ export const createComment =  async (req, res) => {
   });
   video.comments.push(comment._id);
   video.save();
-  return res.sendStatus(201);
+  return res.status(201).json({ newCommentId: comment._id });
 };
